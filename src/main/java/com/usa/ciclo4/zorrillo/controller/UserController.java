604817b8471efa8fad7bf48cc220be6fa4d71e6b
Alix Rincón Carreño <alixshayanna@gmail.com>
2021-12-03 21:35:59 -0500

    se incluye reponseStatus

--- src/main/java/com/usa/ciclo4/zorrillo/controller/UserController.java
+++ src/main/java/com/usa/ciclo4/zorrillo/controller/UserController.java
@@ -13,31 +13,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
-
+/**
+ * @author: Alix Rincón
+ */
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin("*")
 public class UserController {
     
+    /**
+     * UserService 
+     */
     @Autowired
     private UserService userService;
 
+    /**
+     * 
+     * @return UserService
+     */
     @GetMapping("/all")
     public List<User> getAll(){
         return userService.getAll();   
     }    
 
+    /**
+     * 
+     * @param user
+     * @return UserService
+     */
     @PostMapping("/new")
-    public User registeUser(@RequestBody User user) {       
-        return userService.registerUser(user);
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registeUser(@RequestBody User user) {       
+        userService.registerUser(user);
     }
-  
+
+    /**
+   * 
+   * @param email
+   * @param password
+   * @return
+   */
     @GetMapping("/{email}/{password}")
     public User authenticateUser(@PathVariable("email") String email, @PathVariable("password") String password){
         return userService.autenticatheUser(email,password);
     }   
 
+    /**
+     * @return UserService
+     */
     @GetMapping("/{email}")
     public boolean emailExist(@PathVariable("email") String email){
         return userService.emailExist(email);
